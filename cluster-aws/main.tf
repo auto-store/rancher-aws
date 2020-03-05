4c6e17efa3a9144eefcadeeae9253560e4ab4754
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-05 16:19:58 +0000

    resource undeclared fixed

--- cluster-aws/main.tf
+++ cluster-aws/main.tf
@@ -53,10 +53,10 @@ resource "rancher2_node_template" "template" {
 }
 # Create a new rancher2 Node Pool
 resource "rancher2_node_pool" "pool" {
-  cluster_id =  data.cluster.cluster.id
+  cluster_id =  data.rancher2_cluster.cluster.id
   name = var.pool_name 
   hostname_prefix =  "foo-cluster-0"
-  node_template_id = data.template.rancher2_node_template.template.id
+  node_template_id = data.rancher2_node_template.template.id
   quantity = 3
   control_plane = true
   etcd = true

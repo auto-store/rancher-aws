2d52f46589f7bc391f7a644283ddfead0f7663f8
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-06 09:54:03 +0000

    default VPC

--- network-aws/main.tf
+++ network-aws/main.tf
@@ -2,20 +2,31 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-resource "aws_vpc" "tharris_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_default_vpc" "default" {
 }
 
-resource "aws_subnet" "cluster_subnet" {
-  vpc_id     = [aws_vpc.tharris_vpc.id]
-  cidr_block = "10.50.0.0/24"
+resource "aws_default_subnet" "default" {
+    availability_zone = "eu-west-2a"
 
-  depends_on = [aws_vpc.tharris_vpc]
 }
+
+
+
+#resource "aws_vpc" "tharris_vpc" {
+#}
+#  cidr_block = "10.0.0.0/16"
+#resource "aws_subnet" "cluster_subnet" {
+#
+#  vpc_id     = [aws_vpc.tharris_vpc.id]
+#
+#  cidr_block = "10.50.0.0/24"
+#  depends_on = [aws_vpc.tharris_vpc]
+#}
+#
 
 output "vpc_id" {
-    value = [aws_vpc.tharris_vpc.id]
 }
+    value = [aws_default_vpc.default.id]
 
 output "subnet_id" {
     value = [aws_subnet.cluster_subnet.id]

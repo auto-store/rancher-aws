c375f436d6dbf0ccbf83f005798af57cb0144433
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-06 11:22:35 +0000

    added zone

--- cluster-aws/main.tf
+++ cluster-aws/main.tf
@@ -61,6 +61,7 @@ resource "rancher2_node_template" "template" {
     security_group = data.terraform_remote_state.server.outputs.security_group
     vpc_id = data.terraform_remote_state.network-aws.outputs.vpc_id
     subnet_id = data.terraform_remote_state.network-aws.outputs.subnet_id
+    zone = "eu-west-2a"
   }
 }
 # Create a new rancher2 Node Pool
